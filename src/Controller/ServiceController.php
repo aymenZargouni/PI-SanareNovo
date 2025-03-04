2f6a9a1f18f1dc674679b6309a20e7f0b934919d
medmahdy <medmahdy.chaouachi@esprit.tn>
2025-03-04 22:36:15 +0100

    fixed pulling with user

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -8,15 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\ServiceType;
 use App\Repository\ServiceRepository;
-<<<<<<< HEAD
 use App\Repository\SalleRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
-=======
-use Doctrine\Persistence\ManagerRegistry;
-
-
->>>>>>> 76cd849eb886fb88c0dc2f63d25d196cdcb96271
 
 final class ServiceController extends AbstractController{
     #[Route('/service', name: 'app_service')]
@@ -28,7 +22,6 @@ final class ServiceController extends AbstractController{
     }
 
     #[Route('showservice', name: 'showservice')]
-<<<<<<< HEAD
     public function showservice (ServiceRepository $serRep,Request $req): Response
     {
          // Get the search term from the request (if any)
@@ -46,15 +39,6 @@ final class ServiceController extends AbstractController{
         'tabservice' => $services,
         'searchTerm' => $searchTerm,  // Pass the search term to the template
     ]);}
-=======
-    public function showservice (ServiceRepository $serRep): Response
-    {
-        $Blog = $serRep->findAll();
-        return $this->render('service/showservice.html.twig', [
-            'tabservice' => $Blog,
-        ]);
-    }  
->>>>>>> 76cd849eb886fb88c0dc2f63d25d196cdcb96271
 
     #[Route('/addFormservice', name: 'addFormservice')]
     public function addFromservice( ManagerRegistry $m, Request $req): Response
@@ -106,7 +90,6 @@ final class ServiceController extends AbstractController{
         return $this->redirectToRoute('showservice'); // redige vers la liste des auteurs aprés l'ajout  
 
     }
-<<<<<<< HEAD
 
     #[Route('/dashboard', name: 'app_dashboard')]
     public function dashbord (ServiceRepository $serviceRepository, SalleRepository $salleRepository): Response
@@ -148,6 +131,4 @@ final class ServiceController extends AbstractController{
         // Retourner les résultats sous forme de JSON
         return new JsonResponse($salles);
     }
-=======
->>>>>>> 76cd849eb886fb88c0dc2f63d25d196cdcb96271
 }
